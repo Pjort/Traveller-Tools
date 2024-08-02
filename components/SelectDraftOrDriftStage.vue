500e4016c439983a98527106ad6059f6604efea6
Pjort Kat <pjort.kat@gmail.com>
2024-08-02 22:00:04 +0200

    Changed to key instead of id

--- components/SelectDraftOrDriftStage.vue
+++ components/SelectDraftOrDriftStage.vue
@@ -2,9 +2,9 @@
 	<div>
 		<h1 class="p-2">Select Career</h1>
 		<div class="p-2">
-			<div v-for="choice in choices" :key="choice.id">
-				<input type="radio" v-model="selectedChoice" :value="choice.id" :id="choice.id" />
-				<label :for="choice.id">{{ " " + choice.value }}</label>
+			<div v-for="choice in choices" :key="choice.key">
+				<input type="radio" v-model="selectedChoice" :value="choice.key" :id="choice.key" />
+				<label :for="choice.key">{{ " " + choice.value }}</label>
 			</div>
 		</div>
 		<button class="p-2 bg-slate-400" @click="ApplyInput">Apply</button>
@@ -15,9 +15,9 @@
 const characterStore = useCharacterStore();
 const currentTermString = computed(() => characterStore.getCharacterInput.TermsString);
 
-const choices: { id: string; value: string }[] = [
-	{ id: "1", value: "Submit to Draft" },
-	{ id: "2", value: "Become a Drifter" },
+const choices: { key: string; value: string }[] = [
+	{ key: "1", value: "Submit to Draft" },
+	{ key: "2", value: "Become a Drifter" },
 ];
 const selectedChoice = ref<string | null>(null);
 
